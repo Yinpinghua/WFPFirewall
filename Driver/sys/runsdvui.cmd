e83b87dc54b0bf633bb31753cb4bf13d3b0e83f7
yinpinghua <986323930@qq.com>
2021-06-23 15:27:28 +0800

    first commit

---
+++ Driver/sys/runsdvui.cmd
@@ -0,0 +1,2 @@
+cd /d "G:\coding\WFPFirewall-master\Driver\sys" &msbuild "msnmntr.vcxproj" /t:sdvViewer /p:configuration="Debug" /p:platform="x64" /p:SolutionDir="G:\coding\WFPFirewall-master" 
+exit %errorlevel% 
